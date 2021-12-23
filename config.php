6c1278145a732bedf9e8b7ecfb961092b6242825
Donovan <90481137+therealdonovan@users.noreply.github.com>
2021-12-23 14:22:15 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php 
+$apiRepo = "therealdonovan/DonovanTVAPI"; //The API that DonovanTV connects to to get the channels
+$updatePw = "pass"; //The password needed to update to the latest version of DonovanTV
+?>
